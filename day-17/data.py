9cfd516a619f1ac2bba030cacff55d6f083b3a65
Martin Zapletal <martin.zapletal26@gmail.com>
2026-03-28 13:37:28 +0100

    feat: added day-17 folder

---
+++ day-17/data.py
@@ -0,0 +1,12 @@
+question_data = [
+    {"text": "The first dual-core CPU was the Intel Pentium D.", "answer": "False"},
+    {"text": "Linus Sebastian is the creator of the Linux kernel, which went on to be used in Linux, Android, and Chrome OS.", "answer": "False"},
+    {"text": "MacOS is based on Linux.", "answer": "False"},
+    {"text": "To bypass US Munitions Export Laws, the creator of the PGP published all the source code in book form. ", "answer": "True"},
+    {"text": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.", "answer": "False"},
+    {"text": "The very first recorded computer &quot;bug&quot; was a moth found inside a Harvard Mark II computer.", "answer": "True"},
+    {"text": "The &#039;Raspberry Pi&#039; series of single-board computers were created in the United States.", "answer": "False"},
+    {"text": "&quot;Windows NT&quot; is a monolithic kernel.", "answer": "False"},
+    {"text": "The common software-programming acronym &quot;I18N&quot; comes from the term &quot;Interlocalization&quot;.", "answer": "False"},
+    {"text": "A Boolean value of &quot;0&quot; represents which of these words?", "answer": "False"}
+]
